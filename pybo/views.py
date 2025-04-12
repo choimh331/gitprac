8c432339f16ce45e58b4a83e460fb5fda46f76a1
chbasis331 <chbasis331@gmail.com>
2025-04-12 17:38:08 +0900

    fit 오류 생기도록

--- pybo/views.py
+++ pybo/views.py
@@ -9,16 +9,16 @@ from django.core.paginator import Paginator
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
-    question_list = Question.objects.order_by('-create_date')
+    question_list = Question.objects.order_by('create_date')
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_li': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
+    context = {'questi': question}
     return render(request, 'pybo/question_detail.html', context)
 
 def answer_create(request, question_id):
